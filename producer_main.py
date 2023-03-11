b07e756aab7d57456b7b93de62072c3f4f5eb05c
saisubhasish <saisubhasishrout777@gmail.com>
2023-03-11 14:50:24 +0530

    Files added

--- producer_main.py
+++ producer_main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
         sample_topic_data_dir = os.path.join(SAMPLE_DIR,topic)
         sample_file_path = os.path.join(sample_topic_data_dir,os.listdir(sample_topic_data_dir)[0])
         product_data_using_file(topic=topic,file_path=sample_file_path)
+
